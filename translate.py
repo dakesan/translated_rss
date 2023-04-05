77bbd536cc1dabe5365ce5f72c09a70609fa930d
snitch0 <snitch@excel2rlang.com>
2023-04-06 06:41:30 +0900

    natureだと失敗する

--- translate.py
+++ translate.py
@@ -1,5 +1,6 @@
 import xml.etree.ElementTree as ET
 from urllib import request
+import requests
 import os
 
 import deepl
@@ -7,30 +8,34 @@ import deepl
 
 def main():
     # download rss feeds
-    request.urlretrieve("https://www.nature.com/nature.rss", "nature.rss")
-    request.urlretrieve("http://mediagazer.com/feed.xml", "mediagazer.rss")
+    response = requests.get("https://www.nature.com/nature.rss")
+    with open("nature.rss", "w") as file:
+        file.write(response.text)
+    response = requests.get("http://mediagazer.com/feed.xml")
+    with open("mediagazer.rss", "w") as file:
+        file.write(response.text)
 
     # deepl
     API_KEY = os.environ.get("DEEPL_KEY")
     translator = deepl.Translator(API_KEY)
     target_language = "JA"
-
-    # parse Nature
-    tree = ET.parse("nature.rss")
-    root = tree.getroot()
-    items = root.findall("item", {"": "http://purl.org/rss/1.0/"})
-
-    for child in items:
-        result = translator.translate_text(
-            child.findall("title", {"", "http://purl.org/dc/elements/1.1/"})[0].text,
-            target_lang=target_language,
-        )
-
-        child.findall("title", {"", "http://purl.org/dc/elements/1.1/"})[
-            0
-        ].text = result.text
-        child.findall("title", {"": "http://purl.org/rss/1.0/"})[0].text = result.text
-    tree.write("nature.rss", encoding="utf-8", xml_declaration=True)
+    #
+    # # parse Nature
+    # tree = ET.parse("nature.rss")
+    # root = tree.getroot()
+    # items = root.findall("item", {"": "http://purl.org/rss/1.0/"})
+    #
+    # for child in items:
+    #     result = translator.translate_text(
+    #         child.findall("title", {"", "http://purl.org/dc/elements/1.1/"})[0].text,
+    #         target_lang=target_language,
+    #     )
+    #
+    #     child.findall("title", {"", "http://purl.org/dc/elements/1.1/"})[
+    #         0
+    #     ].text = result.text
+    #     child.findall("title", {"": "http://purl.org/rss/1.0/"})[0].text = result.text
+    # tree.write("nature.rss", encoding="utf-8", xml_declaration=True)
 
     # parse mediagazer
     tree = ET.parse("mediagazer.rss")
